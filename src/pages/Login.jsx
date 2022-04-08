2e96f071b6c9bbf648987413d1f1b242e1d60879
Mifta <taajannah99@gmail.com>
2022-04-08 01:40:14 -0700

    Mengubah Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,34 @@
+import { Form, Button } from "react-bootstrap";
+
 function LoginPage() {
     return (
-        <div>
-            Login Page
-        </div>
-    )
+        <container>
+            <div className="row row-cols-2">
+                <div>
+
+                </div>
+
+                <div>
+                <H1 className="text-center mb-3"> Login</H1>
+            <Form> 
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+                </div>
+            </div>
+        </container>
+
+    );
 }
 
-export default LoginPage
+export default LoginPage;
