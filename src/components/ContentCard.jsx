2d01ce6a675e56efc7e2046c2c257b120beda965
adma74 <tagahnuku@gmail.com>
2022-04-09 19:30:06 +0800

    add navigate to detail in card

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -1,12 +1,20 @@
 import { Row, Col, Card, Button } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom"
+
 
 function ContentCard() {
+    const navigate = useNavigate();
+
+    const movePage = (path) => {
+        navigate(path);
+    };
+    
     return (
         <div>
             <Row xs={2} md={4} className="g-4">
                 {[1, 2, 3, 4].map((_, idx) => (
                     <Col>
-                        <Card style={{ width: '18rem' }}>
+                        <Card style={{ width: '18rem' }} onClick={() => movePage("/details")}>
                             <Card.Img variant="top" src="holder.js/100px160" />
                             <Card.Body>
                                 <Card.Title>Card title</Card.Title>
