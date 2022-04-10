84efc12884a98643337cb76eb2c113a4bc1ea38c
adma74 <tagahnuku@gmail.com>
2022-04-11 04:08:22 +0800

    update CardDetail.jsx CardHome.jsx, ContentCard.jsx FooterComp.jsx

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -2,16 +2,22 @@ import { Card } from 'react-bootstrap'
 
 function CardDetail() {
     return (
-        <Card className="bg-dark text-white" style={{ width: '845px'}}>
-            <Card.Img src="https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg" alt="Card image" />
-            <Card.ImgOverlay>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                </Card.Text>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
+        <Card className="bg-dark text-white" style={{ margin: '50px', display: 'flex', flexDirection: 'row' }} >
+            <div>
+                <Card.Img className="img-fluid rounded-start" src="https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg" alt="Card image" />
+            </div>
+            <div style={{ marginLeft: '20px' }}>
+                <Card.Title>Movie title</Card.Title>
+                
+                <Card.Text>Producer </Card.Text>
+                <Card.Text>Genre </Card.Text>
+                <Card.Text>Artis </Card.Text>
+                <Card.Text>Date Release </Card.Text>
+                <Card.Text>Official Trailer </Card.Text>
+                <Card.Text>Rating </Card.Text>
+                <Card.Text>See Full Movie <a href='https://www.youtube.com/watch?v=ltawe4MeLxs'>here</a></Card.Text>
+                <Card.Text>Synopsis </Card.Text>
+            </div>
         </Card>
     )
 }
--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from "react-bootstrap"
 import { useNavigate } from 'react-router-dom'
-import "./Card.css"
+import '../assets/css/Card.css'
 
 function CardHome() {
     const navigate = useNavigate();
