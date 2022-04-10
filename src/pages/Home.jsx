e07bcdd3f0863c5679a004529827d8f39746c1a8
adma74 <tagahnuku@gmail.com>
2022-04-10 19:27:57 +0800

    update CardHome.jsx CarouselHome.jsx NavbarComp.jsx Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,26 @@
 import CardHome from "../components/CardHome"
 import CarouselHome from "../components/CarouselHome"
-
-import { Row, Col, Button } from "react-bootstrap"
+import { Row, Col, Button, Container } from "react-bootstrap"
+import { useNavigate } from 'react-router-dom'
 
 function HomePage() {
-    return (
-        <div>
-            <Row>
-                <Col> <CarouselHome /></Col>
-                <Col> <CardHome /> </Col>
-            </Row>
-            <Button style={{ margin: "0px 550px", backgroundColor: "black", border: "2px black" }}>See More Recomendations</Button>
+    const navigate = useNavigate();
 
+    const movePage = (path) => {
+        navigate(path);
+    };
+
+    return (
+        <div style={{ marginTop: '50px' }}>
+            <Container>
+                <Row>
+                    <Col> <CarouselHome /></Col>
+                    <Col> <CardHome /> </Col>
+                </Row>
+            </Container>
+            <Button style={{ margin: "100px", backgroundColor: "black", border: "2px black" }}>
+                <h6 onClick={() => movePage("/movies")}> See More Recomendations </h6>
+            </Button>
         </div>
     )
 }
