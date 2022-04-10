7aa44ab87ab5bc7de2a9ae5fa12172c1a99f6a1c
adma74 <tagahnuku@gmail.com>
2022-04-11 04:13:47 +0800

    change imgUrl CardHome.jsx CarouselHome.jsx

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -15,7 +15,7 @@ function CarouselHome() {
                     <Carousel.Item onClick={() => movePage("/details")}>
                         <img
                             className="d-block"
-                            src="https://img.celebrities.id/okz/900/76DBm2/master_1X0U6h9ec1_1090.jpg"
+                            src="https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg"
                             alt="First slide"
                         />
                         <Carousel.Caption>
@@ -26,7 +26,7 @@ function CarouselHome() {
                     <Carousel.Item onClick={() => movePage("/details")}>
                         <img
                             className="d-block"
-                            src="https://imgsrv2.voi.id/frOH7qzW_irTEb3JH73crBMy2Fv8jtw1OooHUweTtoU/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy85OTI3NjYzYy03NGYyLTQyZWMtYWIwZi02NjE3ZDJlOWQ0ZGIvMjAyMDA0MDgxMTA5LW1haW4uY3JvcHBlZF8xNTg2MzE4OTcyLmpwZWc.jpg"
+                            src="https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg"
                             alt="Second slide"
                         />
 
@@ -38,7 +38,7 @@ function CarouselHome() {
                     <Carousel.Item onClick={() => movePage("/details")}>
                         <img
                             className="d-block"
-                            src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/09/01/1054189481.jpeg"
+                            src="https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg"
                             alt="Third slide"
                         />
 
