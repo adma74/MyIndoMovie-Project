0c83268cff8b68d577357d82fb0bab825ff5b037
Mifta <taajannah99@gmail.com>
2022-04-11 00:49:51 -0700

    Menambahkan branch Mifta/dev2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,80 @@
 import { Form, Button } from "react-bootstrap";
+import { useNavigate, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+
 
 function LoginPage() {
+    const navigate = useNavigate()
+    const [ form, setForm ] = useState({
+        email: '',
+        password: ''
+    }) 
+    const location = useLocation()
+    const stateLocation = location?.state
+
+    const changeEmail = (e) => {
+        const value = e.target?.value
+
+        setForm({
+            ...form,
+            email: value
+        })
+    }
+
+    const changePassword = (e) => {
+        const value = e.target?.value
+
+        setForm({
+            ...form,
+            password: value
+        })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (form.email && form.password) {
+            fetch ('https://62535ce690266e3d0e02674e.mockapi.io/user')
+            .then(response => response.json())
+            .then(data => {
+                const findUser = data?.find(user => (user?.email === form.email) && (user?.password === form.password))
+                if (findUser) {
+                    console.log('Berhasil Login')
+                    toast.success('Success Login !', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        progress: undefined,
+                    })
+                    localStorage.setItem('userLogin', form.email)
+                    localStorage.setItem('userLoginID', findUser.id)
+                    const fromLocation = stateLocation?.from?.pathname
+                    if (fromLocation) {
+                        navigate(fromLocation)
+                    } else {
+                        navigate('/')
+                    }
+                } else {
+                    console.log('Gagal Login')
+                    toast.error('Invalid Auth !', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    })
+                }
+            })
+            .catch(err => console.log(err))
+        }
+    }
     return (
-        <container>
-            <div className="row row-cols-2">
-                <div>
-
-                </div>
-
-                <div>
-                    <h1 className="text-center mb-3"> Login</h1>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </div>
-            </div>
-        </container>
+        
+        <FormLogin />
 
     );
 }
